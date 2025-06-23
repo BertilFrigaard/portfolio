d697751c0d6d1ece6e091a61fe5251f0a8c7b05a
BertilF <bertilfrigaard@gmail.com>
2025-06-23 20:12:14 +0200

    Added burger menu functionality

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Link from "next/link";
 import BouncingLink from "../BouncingLink/BouncingLink";
 import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 
 export default function Header() {
+    const [menuActive, setMenuActive] = useState(false);
     return (
         <header>
             <nav className="m-6 md:m-10 flex justify-between items-center">
@@ -26,8 +29,27 @@ export default function Header() {
                         Contact
                     </BouncingLink>
                 </div>
-                <div className="md:hidden text-4xl">
-                    <IoMenu />
+                <div className="md:hidden">
+                    <div className="relative inline-block">
+                        <IoMenu className="text-4xl cursor-pointer" onClick={() => setMenuActive(!menuActive)} />
+
+                        {menuActive && (
+                            <div className="absolute right-0 mt-2 bg-white shadow-md rounded-lg p-4 space-y-2 text-lg z-50 pl-5 pr-10">
+                                <BouncingLink href="/#about" className="hover:underline">
+                                    About
+                                </BouncingLink>
+                                <BouncingLink href="/#projects" className="hover:underline">
+                                    Projects
+                                </BouncingLink>
+                                <BouncingLink href="/#experience" className="hover:underline">
+                                    Experience
+                                </BouncingLink>
+                                <BouncingLink href="/#contact" className="hover:underline">
+                                    Contact
+                                </BouncingLink>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </nav>
         </header>
